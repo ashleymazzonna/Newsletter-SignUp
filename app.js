f051aa0eb1f8dafdb267b8e2b6178c61c6bf3ef3
Ashley <31573757+ashleymazzonna@users.noreply.github.com>
2020-01-11 16:26:51 -0500

    Add js boilerplate

---
+++ app.js
@@ -0,0 +1,22 @@
+//jshint esversion:6
+
+const express = require('./node_modules/express');
+const bodyParser = require("./node_modules/body-parser");
+
+const app = express();
+
+
+app.get("/", function (req, res) {
+
+
+
+
+
+
+});
+
+app.listen(3000, function () {
+    console.log("server is running on port 4000");
+});
+
+//Get API key from MailChimop
