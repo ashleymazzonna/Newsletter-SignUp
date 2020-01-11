597f3391c88399447112e9fd26ef220b77e59207
Ashley <31573757+ashleymazzonna@users.noreply.github.com>
2020-01-11 17:52:11 -0500

    update to UI. Setup express and body-parser

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 //jshint esversion:6
 
-const express = require('./node_modules/express');
-const bodyParser = require("./node_modules/body-parser");
+// const express = require('./node_modules/express');
+// const bodyParser = require("./node_modules/body-parser");
 
-const app = express();
-
-
-app.get("/", function (req, res) {
+// const app = express();
 
 
+// app.get("/", function (req, res) {
 
 
+// });
+
+// app.listen(3000, function () {
+//     console.log("server is running on port 4000");
+// });
+
+//Get API key from MailChimp
 
 
-});
-
-app.listen(3000, function () {
-    console.log("server is running on port 4000");
-});
-
-//Get API key from MailChimop
