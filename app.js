3a6e3a0ba00224504264ba88d3d7cedbf78f521f
Ashley <31573757+ashleymazzonna@users.noreply.github.com>
2020-01-13 23:31:07 -0500

    update UI. Set up server

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 //jshint esversion:6
 
-// const express = require('./node_modules/express');
-// const bodyParser = require("./node_modules/body-parser");
+const express = require('./node_modules/express');
+const bodyParser = require("./node_modules/body-parser");
 
-// const app = express();
-
-
-// app.get("/", function (req, res) {
+const app = express();
 
 
-// });
+app.get("/", function (req, res) {
 
-// app.listen(3000, function () {
-//     console.log("server is running on port 4000");
-// });
+
+});
+
+app.listen(3000, function () {
+    console.log("server is running on port 4000");
+});
 
 //Get API key from MailChimp
 
