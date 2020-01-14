7c51a538511a2a0844a25ab95fd2725b6df634a9
Ashley <31573757+ashleymazzonna@users.noreply.github.com>
2020-01-14 11:59:24 -0500

    setup server

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
 //jshint esversion:6
+// 7b3b90bd62849b80421c1cbfc807ff48-us4 mailchimp api key
 
 const express = require('./node_modules/express');
 const bodyParser = require("./node_modules/body-parser");
+const request = require("./node_modules/request");
 
 const app = express();
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 
 app.get("/", function (req, res) {
-
+    res.sendFile(__dirname + "/index.html")
 
 });
 
-app.listen(3000, function () {
+app.post("/", function(req, res) {
+    let first_name, last_name, Dob, email, password;
+
+    first_name = req.body.fname;
+    email = req.body.email;
+    password = req.body.password;
+
+    console.log(`User first name: ${first_name}. User's email address: ${email}. User password: ${password}`);
+});
+
+
+app.listen(4000, function () {
     console.log("server is running on port 4000");
 });
 
-//Get API key from MailChimp
+
+
 
 
